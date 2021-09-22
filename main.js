458ba3543ef83cd8c133d3e8435d706f63d48146
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-09-22 09:52:28 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var marks = prompt("Enter Your Marks :");
+ if (marks >= 80)
+   document.write("Congratuation! You😍🤗 got  A+");
+   
+else if (marks >= 70)
+   document.write("Congratuation! You🤩 got  A ");
+   
+else if (marks >= 60)
+   document.write("Congratuation! You😇 got  A- ");
+   
+   else if (marks >= 50)
+   document.write("Congratuation! 😉You got B ");
+   
+   else if (marks >= 40)
+   document.write("Congratuation! ☺️You got C ");
+   else if (marks >= 33)
+   document.write("Congratuation!😊 You got 'D' ");
+   
+else{
+   document.write("Sorry Dear!😔 You should try Again");
+   }
